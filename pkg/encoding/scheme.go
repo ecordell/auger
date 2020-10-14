2724d5c982dd2346b03ab322e379544aa469deac
Evan <cordell.evan@gmail.com>
2020-10-14 09:50:13 -0400

    bump to kube 1.18 schemas

--- pkg/encoding/scheme.go
+++ pkg/encoding/scheme.go
@@ -17,11 +17,9 @@ limitations under the License.
 package encoding
 
 import (
-	"os"
-
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
 
-	admissionregistrationv1alpha1 "k8s.io/api/admissionregistration/v1alpha1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 
 	appsv1 "k8s.io/api/apps/v1"
@@ -67,14 +65,12 @@ import (
 	storagev1alpha1 "k8s.io/api/storage/v1alpha1"
 	storagev1beta1 "k8s.io/api/storage/v1beta1"
 
-	"k8s.io/apimachinery/pkg/apimachinery/registered"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 )
 
-var Registry = registered.NewOrDie(os.Getenv("KUBE_API_VERSIONS"))
 var Scheme = runtime.NewScheme()
 var Codecs = serializer.NewCodecFactory(Scheme)
 var ParameterCodec = runtime.NewParameterCodec(Scheme)
@@ -101,7 +97,7 @@ func init() {
 func AddToScheme(scheme *runtime.Scheme) {
 	admissionv1beta1.AddToScheme(scheme)
 
-	admissionregistrationv1alpha1.AddToScheme(scheme)
+	admissionregistrationv1.AddToScheme(scheme)
 	admissionregistrationv1beta1.AddToScheme(scheme)
 
 	appsv1.AddToScheme(scheme)
